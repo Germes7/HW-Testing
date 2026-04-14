70b6741e8e93401a4e6b53ea4ff72ce5857a9393
Derek <ushakov_gd@mail.ru>
2026-04-14 13:05:48 +0300

    Написал класс LibraryBook

--- HW_2_Testing/functions_hw2.py
+++ HW_2_Testing/functions_hw2.py
@@ -49,7 +49,6 @@ class Temperature:
         elif t1.__celsius < t2.__celsius: return 1
         elif t1.__celsius == t2.__celsius: return 0
 
-
 # temp_1 = Temperature(0)
 # temp_2 = Temperature(-273)
 # print("-" * 20)
@@ -65,3 +64,73 @@ class Temperature:
 # print("-" * 20)
 # print(temp_1.compare(temp_1, temp_2))
 # print(temp_2.compare(temp_1, temp_2))
+
+
+# Задача №2
+# Создайте класс LibraryBook, описывающий книгу в библиотеке.
+# У класса должен быть:
+# конструктор, принимающий название, автора и год издания Library(title: str,
+# author: str, publish_year: int).
+# Методы доступа к названию, автору и году.
+# Метод rename(new_title), меняющий название книги.
+# Метод info(), возвращающий строку с краткой информацией о книге.
+# Методы ниже в качестве параметра принимают текущий год!
+# Метод is_old(current_year), проверяющий, является ли книга старше 50 лет.
+# Метод age(current_year), возвращающий возраст книги.
+
+class LibraryBook:
+
+    __title: str
+    __autor: str
+    __year_publication: int
+
+    def __init__(self, title: str, autor: str, year_publication: int):
+        if not isinstance(title, str):
+            raise TypeError("type: str")
+
+        self.__title = title
+        self.__autor = autor
+        self.__year_publication = year_publication
+
+    def get_title(self):
+        return self.__title
+
+    def get_autor(self):
+        return self.__autor
+
+    def get_year_publication(self):
+        return self.__year_publication
+
+    def rename_title(self, new_title: str):
+
+        if not isinstance(new_title, str):
+            raise TypeError("type: str")
+
+        if new_title != self.__title:
+            self.__title = new_title
+
+    def info(self):
+        return f"Книга: {self.__title}. Автор: {self.__autor}. Год издания: {self.__year_publication} г."
+
+    def is_old(self, current_year: int):
+
+        if not isinstance(current_year, int):
+            raise TypeError("type: int")
+
+        if current_year - self.__year_publication > 50:
+            return True
+
+        return False
+
+    def age(self, current_year: int):
+
+        if not isinstance(current_year, int):
+            raise TypeError("type: int")
+        return current_year - self.__year_publication
+
+# book = LibraryBook("Аэлита", "А.Н. Толстой", 1923)
+# print(book.info())
+# book.rename_title("Гиперболоид инженера Гарина")
+# print(book.info())
+# print(book.is_old(2026))
+# print(book.age(2026))
